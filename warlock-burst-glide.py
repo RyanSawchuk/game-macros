398a86b5c96df41cf0fd38c77c637733065da8e8
Ryan <ryan.sawchuk@dal.ca>
2022-09-14 23:07:35 -0700

    update burst glide to sword skate

--- warlock-burst-glide.py
+++ warlock-burst-glide.py
@@ -4,9 +4,8 @@ warnings.filterwarnings("ignore")
 import os, sys
 
 import keyboard as kb
-from pynput import mouse
-from pynput.mouse import Listener
 import pyautogui
+import time
 
 pyautogui.PAUSE = 0.05
 
@@ -15,21 +14,26 @@ def usage():
     sys.exit(0)
 
 
-def burst():
+def swordskate():
+    print("swordskate()")
+    #pyautogui.press('3')
+    #time.sleep(0.15)
     pyautogui.press('space')
+    time.sleep(0.15)
+    pyautogui.click()
+    time.sleep(0.15)
     pyautogui.press('space')
-    pyautogui.press('space')
-    pyautogui.press('space')
-    #pyautogui.press('space')
+    time.sleep(0.15)
+    pyautogui.press('h')
 
 
 def on_scroll(x, y, dx, dy):
-    burst()
+    swordskate()
 
 
 def on_click(x, y, button, pressed):
     if str(button) == 'Button.middle':
-        burst()
+        swordskate()
 
 
 key = sys.argv[1]
@@ -38,12 +42,12 @@ stopkey = sys.argv[2]
 if not key or not stopkey:
 	usage()
 
-while False:
+while True:
 	if kb.is_pressed(key):
-		burst()
+		swordskate()
 
 	if kb.is_pressed(stopkey):
 		sys.exit(0)
 
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+# with mouse.Listener(on_click=on_click) as listener:
+#     listener.join()
